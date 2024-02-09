7d98fb4bdadb42084ee887804ceec7bde109f9e2
TheGodzGaming <159502695+TheGodzGaming@users.noreply.github.com>
2024-02-09 10:28:48 -0500

    Add files via upload

---
+++ godz-newped_wpolyzone/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.displayText = false -- Whether you want the heading text to show or not
+Config.displayDistance = 20.0 -- From how far to start displaying text
+Config.displayColor = "~g~"
+
+Config.Peds = {
+    -- { x, y, z, ped heading, model hash, ped model, heading text, animation info }
+    {309.9150, -601.7606, 43.2919, 153.7847, 0x2D0EFCEB,"u_m_y_cyclist_01", "Cyclist", "amb@world_human_aa_smoke@male@idle_a"}
+}
+
+Config.DebugPoly = false
+
---
+++ godz-newped_wpolyzone/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'TheGodz'
+description 'NPC Spawning Script With QB-Target on Polyzone, Selection that Triggers Events'
+version '2.0.0'
+repository 'https://github.com/TheGodzGaming/5M-godz-newped-wpolyzone'
+
+
+-- What to run
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+export 'addNPC'
